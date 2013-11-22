c30084652e388c2b79ef405b82d9557c8496a0c1
Maurizio Rendon <alu0100611724@ull.edu.es>
2013-11-22 10:14:56 +0000

    "Prueba"

--- src/complementos/Main.java
+++ src/complementos/Main.java
@@ -8,6 +8,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		
 		JuegoDelLaberinto fabricaLab = new JuegoDelLaberintoEncantado();
 		System.out.println("\nHogwarts Maze\n");
 		Laberinto hogwarts = fabricaLab.crearLaberinto();
